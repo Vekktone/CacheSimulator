8f2c8c527d8a7a0f56cf1ac8e68d419145f43dd4
ros442 <ros442@gnu008.cs.utsarr.net>
2018-11-20 19:48:52 -0600

    Added file read

--- cacheSimulator.py
+++ cacheSimulator.py
@@ -1,1 +1,10 @@
+import sys
+
+file = open(sys.argv[1], "r")
+while True:
+    inputline = file.readline()
+    if inputline == "":
+        break
+    print(inputline)
+
 print("This is a cache simulator")
